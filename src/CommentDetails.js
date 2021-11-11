82304991eb1986bf5e9788ef8a8de3ddeb7df7e4
Tejas Throat <Tejsrelax@gmail.com>
2021-11-11 23:52:56 +0530

    foiling around

--- src/CommentDetails.js
+++ src/CommentDetails.js
@@ -1,20 +1,23 @@
 import React from "react";
 import faker from "faker";
 
-const CommentDetails = () => {
+const CommentDetails = (props) => {
+  console.log(props);
   return (
     <div className="comment">
       <a className="avatar" href="/">
-        <img src={faker.image.avatar()} alt="" />
+        <img src={props.avatar} alt="avatar" />
       </a>
       <div className="content">
         <a className="author" href="/">
-          {faker.internet.userName()}
+          {props.author}
         </a>
         <div className="metadata">
           <div className="date">{`${faker.datatype.datetime()}`}</div>
         </div>
-        <div className="text">{faker.company.catchPhrase()}</div>
+
+        <div className="text">{props.text}</div>
+        {/* <div className="text">{faker.company.catchPhrase()}</div> */}
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,14 +2,27 @@
 import React from "react";
 import ReactDom from "react-dom";
 import CommentDetails from "./CommentDetails";
+import faker from "faker";
 
 //create
 const App = () => {
   return (
     <div className="ui comments">
-      <CommentDetails />
-      <CommentDetails />
-      <CommentDetails />
+      <CommentDetails
+        author="ayush"
+        text="snehal didi"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetails
+        author="tapasvi"
+        text="yedi"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetails
+        author="soham"
+        text="nahiye"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
